e05bfe7613518a3d86e23c2b815ba7b895c02613
Iyed <iyed.bennour@gmail.com>
2016-03-23 19:43:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-  vals := make([]string, 3)
+
+	vals := make([]string, 3)
+	vals[0] = "foo"
+	vals[1] = "bar"
+	vals[2] = "foobar"
+
+	fmt.Printf("%v\n", vals)
+
 }
